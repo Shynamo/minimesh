5b666e0240481a89f6ca5bc5d20f607833c81bab
ROSEAU Baptiste <broseau@enseirb-matmeca.fr>
2019-12-12 16:17:19 +0100

    Added a 'file_exists' function

--- includes/misc.hpp
+++ includes/misc.hpp
@@ -5,3 +5,4 @@
 #include <vector>
 
 bool has_extension(std::string file_name, std::string extension);
+bool file_exists(const std::string& name);
--- src/misc.cpp
+++ src/misc.cpp
@@ -3,7 +3,15 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
+#include <sys/stat.h>
 
-bool has_extension(std::string file_name, std::string extension){
+using namespace std;
+
+bool has_extension(string file_name, string extension){
   return file_name.substr(file_name.find_last_of(".") + 1) == extension;
+}
+
+bool file_exists(const string& name){
+  struct stat buffer;   
+  return (stat (name.c_str(), &buffer) == 0); 
 }
